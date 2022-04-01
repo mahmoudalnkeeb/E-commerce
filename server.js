aa3677aab11d40ebcf19912258c08c80eb8947c9
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-04-01 06:45:45 +0200

    public  css / js / images

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes/routes.js');
 const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const app = express();
 dotenv.config();
 
@@ -18,7 +19,8 @@ module.exports = { mongoose };
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+console.log(path.join(__dirname, './public'));
+app.use(express.static(path.join(__dirname, './public')));
 app.set('view engine', 'ejs');
 app.use(errHandler);
 app.use(routes);
